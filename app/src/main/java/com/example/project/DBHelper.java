31a58dc24fb7d2da04473e3d4d031103f4d2117a
Raichal <raichalvarghese231@gmail.com>
2022-12-28 23:37:40 +0530

    initial commit

---
+++ app/src/main/java/com/example/project/DBHelper.java
@@ -0,0 +1,47 @@
+package com.example.project;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+    public DBHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, "car.db",null,1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("Create table Users(name Text,contact Text primary key,email Text)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("drop Table if exists Users");
+    }
+
+    public boolean insertdata(String name,String contact,String email){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("name",name);
+        cv.put("contact",contact);
+        cv.put("email",email);
+        long result = db.insert("Users",null,cv);
+        if(result==-1){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+
+    public Cursor getdata(){
+        SQLiteDatabase db = getWritableDatabase();
+        Cursor cursor = db.rawQuery("Select * from Users",null);
+        return cursor;
+    }
+}
---
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button login,signup;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.login);
+        signup = findViewById(R.id.signup);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+    }
+}
